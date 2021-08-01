51cbdc349c83a68d8fdaa7b540bd72fd2b1d6dfd
tonmoy1996 <saha.tonmoy1996@gmail.com>
2021-08-01 22:25:39 +0600

    varibale and git command

--- variable.php
+++ variable.php
@@ -1,3 +1,70 @@
 <?php
 
-echo "Hello World";
+//single line comment
+/*
+multi line comment
+*/
+
+// print
+
+// echo "<h2>Using echo</h2>" . "<br>";
+
+// print "using print " . "<br>";
+
+// print("using print function");
+
+//what is variable
+// is a container which contain some value 
+
+//variable declaration
+$name = "Tonmoy";
+$num = 100;
+$float = 10.2;
+$isActive = true;
+
+//print variable
+
+// echo "name is:" . $name . "<br>";
+// echo "Number:" . $num . "<br>";
+// echo $float;
+
+// //print variable type
+
+// echo getType($name) . "<br>";
+// echo getType($num) . "<br>";
+// echo getType($isActive) . "<br>";
+
+
+//print the whole variable
+
+// var_dump($name);
+// var_dump($num);
+
+// change the variable 
+
+// $name = "Sonju";
+
+// echo "Changed Name:" . $name . "<br>";
+
+//variable checking function
+
+// echo is_string($num);
+// echo is_integer($num);
+// is_float
+// is_double
+// is_bool
+
+//check if variable is defined 
+
+// echo isset($name);
+
+// constant variable 
+
+// define("PI", 3.14);
+
+// echo PI;
+
+
+// build in variable
+
+// echo PHP_INT_MAX;

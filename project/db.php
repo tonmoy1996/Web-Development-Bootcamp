5173ab01a380e60d1f9e6e83e88e1fc8fda501e5
tonmoy1996 <saha.tonmoy1996@gmail.com>
2021-10-04 23:24:48 +0600

    project intialize

---
+++ project/db.php
@@ -0,0 +1,14 @@
+<?php
+$server   = "localhost";
+$username = "root";
+$password = "";
+$database = "ecommerce";
+
+$con = new mysqli($server, $username, $password, $database);
+
+/*if($con->connect_error){
+    echo "Connection Error";
+} else{
+    echo "Connection Successful";
+}*/
+
